eac5dbf722536e9940043924898d14d517dbb71a
santiago <santigarcia2028@gmail.com>
2023-07-04 20:19:47 -0300

    js final preentrega

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,18 +1,24 @@
 // JavaScript Document
 
 function comprar() {
-	let validado = false
-do{
-	let productoseleccionado = prompt ("ingrese el producto que desea comprar")
-	if (productoseleccionado === "silicona","reloj","auriculares") {
-	validado = true
-		alert ("el prosucto que usted va a comprar es " + productoseleccionado)
-	
-	}
+	let validado = false;
 
- else{
-	 alert("el producto que eligio no existe o no esta disponible")
- }
-}while (!validado)
+	do {
+		let productoseleccionado = prompt("Ingrese el producto que desea comprar");
+
+		if (productoseleccionado === "disco") {
+			validado = true;
+			alert("El producto que usted va a comprar es " + productoseleccionado + " por un valor de $350");
+		} else if (productoseleccionado === "reloj") {
+			validado = true;
+			alert("El producto que usted va a comprar es " + productoseleccionado + " por un valor de $5000");
+		} else if (productoseleccionado === "auriculares") {
+			validado = true;
+			alert("El producto que usted va a comprar es " + productoseleccionado + " por un valor de $2800");
+		} else {
+			alert("El producto que eligió no existe o no está disponible");
+		}
+	} while (!validado);
 }
-comprar()
+
+comprar();
